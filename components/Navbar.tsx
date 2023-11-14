770bed666e3e72a545c6f4fb362890468f8981d6
main_character <97624417+rxxddy@users.noreply.github.com>
2023-11-14 12:35:10 +0100

    2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,15 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 
+import {
+    ThirdwebProvider,
+    metamaskWallet,
+    coinbaseWallet,
+    walletConnect,
+    trustWallet,
+    darkTheme,
+  } from "@thirdweb-dev/react";
+
 export default function Navbar() {
     const address = useAddress();
     const disconnect = useDisconnect();
@@ -28,8 +37,8 @@ export default function Navbar() {
     return (
 
         <nav className="bg-[#252525] fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <Link href="/" className="flex items-center">
+            <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+                <Link href="/" className="flex items-center md:hidden ">
                     <img src="https://lh3.googleusercontent.com/u/5/drive-viewer/AK7aPaAAFFvu1f155K_4R1TcVA3vUS7y2BEByU5ws4h5nnd0LNoR-1XLDzkpNYD5h5COd7RYeHa6p5ulgb7nuSe6nT1aY0L38g=w2560-h1243" className="h-8 mr-3" alt="Flowbite Logo"/>
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">The Sybil Market</span>
                 </Link>
@@ -45,7 +54,16 @@ export default function Navbar() {
                         {!address ? (
                             <ConnectWallet 
                                 btnTitle="Connect Wallet"
-                                theme="light"
+                                theme={darkTheme({
+                                    colors: {
+                                      accentText: "#ffb433",
+                                      accentButtonBg: "#ffb433",
+                                    },
+                                  })}
+                                  auth={{ loginOptional: false }}
+                                  switchToActiveChain={true}
+                                  modalSize={"wide"}
+                                
                                 className="text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800"
                             />
                         ) : (
@@ -56,8 +74,10 @@ export default function Navbar() {
                             </div>
                         )}
                     </div>
+          
                     {isProfileDropdownOpen && (
                         <div className="transform translate-y-10 -translate-x-10 z-50 fixed my-4 text-base list-none bg-[#282828] divide-y divide-gray-100 rounded-lg shadow  dark:divide-gray-600" id="user-dropdown">
+                            
                             <div className="px-4 py-3 w-[10em] flex justify-center">
                                 <span className="block text-sm text-gray-900 dark:text-white">{formattedAddress}</span>
                                 {/* <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span> */}
@@ -77,6 +97,10 @@ export default function Navbar() {
                     )}
                 </div>
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    <Link href="/" className="sm:flex hidden items-center mr-20">
+                                <img src="https://lh3.googleusercontent.com/u/5/drive-viewer/AK7aPaAAFFvu1f155K_4R1TcVA3vUS7y2BEByU5ws4h5nnd0LNoR-1XLDzkpNYD5h5COd7RYeHa6p5ulgb7nuSe6nT1aY0L38g=w2560-h1243" className="h-8 mr-3" alt="Flowbite Logo"/>
+                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">The Sybil Market</span>
+                    </Link>
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-[#252525] md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
                     {/* <li>
                         <p className="block py-2 pl-3 pr-4 text-white bg-yellow-700 rounded md:bg-transparent md:text-yellow-700 md:p-0 md:dark:text-yellow-500" aria-current="page">Home</p>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,15 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 
+import {
+
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  trustWallet,
+  darkTheme,
+} from "@thirdweb-dev/react";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
@@ -14,6 +23,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       activeChain={activeChain}
       // clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
       clientId="9e4314f9cb80713a98f3221cfb883eaf"
+      
+      supportedWallets={[
+        metamaskWallet({ recommended: true }),
+        coinbaseWallet(),
+        walletConnect(),
+        trustWallet(),
+      ]}
     >
       <Navbar />
       <Component {...pageProps} />
